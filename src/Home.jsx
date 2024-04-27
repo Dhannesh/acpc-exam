0c204f6dc72111b3e74489100467e1ecdd9db3ef
Dhannesh <dhannesh@gmail.com>
2024-04-27 16:40:02 +0530

    home page updated again

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,9 @@ const Home = () => {
       <h2 className="text-5xl">Department of Computer Science</h2>
       <h2 className="text-4xl m-2">ACPC 2K24</h2>
       <h4 className="font-bold">Conquer The Code!</h4>
-      <br />
+      <h3 className="font-bold text-red-600 mb-4">
+        ABES Collegiate Programming Contest
+      </h3>
       <a
         href={testLink}
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg mt-3 px-5 py-2.5">
