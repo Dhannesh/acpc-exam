f5ac76a4fbb5ea7d2a9cb327eb5b6f145c9b5f69
Dhannesh <dhannesh@gmail.com>
2024-05-02 16:24:31 +0530

    hackerrank link updated

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,3 +1,2 @@
 // export const testLink = "javascript:void(0)";
-export const testLink =
-  "https://codeforces.com/contestInvitation/f27ad73f6b64080de42373ee7a2cff179e9960d6";
+export const testLink = "https://www.hackerrank.com/demo-st-1714646795";
