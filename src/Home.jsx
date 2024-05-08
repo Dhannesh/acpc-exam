ae1ffcb26c0a6957d473d3600ab82c8ee25cc62f
Dhannesh <dhannesh@gmail.com>
2024-05-08 14:54:56 +0530

    Mca exam scheduled

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,19 +1,25 @@
 import { testLink } from "./assets/data";
-import acpc from "./assets/acpc-logo.jpg";
+import mca from "./assets/mca-back.jpg";
 const Home = () => {
   return (
-    <section className="bg-gray-200 text-center p-5 w-full h-auto">
-      <h2 className="text-5xl">ABES Engineering College</h2>
-      <h2 className="text-4xl m-2">Master of Computer Application</h2>
+    <section className="bg-gray-200 text-center p-1 w-full h-auto">
+      <h2 className="text-4xl m-1">Master of Computer Application</h2>
 
       <h3 className="font-bold text-red-600 mb-8 text-3xl">
-        Problem Solving Test!
+        Pre-Placement Preparatory Assessment-1
       </h3>
       <a
         href={testLink}
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg mt-5 px-5 py-2.5">
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg mt-3 px-5 py-2.5">
         Start Assessment
       </a>
+      <center>
+        <img
+          src={mca}
+          className="rounded-md shadow-sm mt-2"
+          style={{ width: 500 }}
+        />
+      </center>
     </section>
   );
 };
--- src/assets/data.js
+++ src/assets/data.js
@@ -1,2 +1,2 @@
 // export const testLink = "javascript:void(0)";
-export const testLink = "https://www.hackerrank.com/demo-st-1714646795";
+export const testLink = "https://forms.office.com/r/xbYf62PDJB";
