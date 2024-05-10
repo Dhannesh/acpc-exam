b8ec95e689e8530db008c4b451db841e808e6272
Dhannesh <dhannesh@gmail.com>
2024-05-10 13:29:06 +0530

    test closed

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,2 +1,2 @@
-// export const testLink = "javascript:void(0)";
-export const testLink = "https://www.hackerrank.com/ps3-ct-1";
+export const testLink = "javascript:void(0)";
+// export const testLink = "https://www.hackerrank.com/ps3-ct-1";
