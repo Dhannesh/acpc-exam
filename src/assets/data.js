f4366637fa55beacc5a4b4f8060a67f2a7869333
Dhannesh <dhannesh@gmail.com>
2024-05-10 11:10:57 +0530

    data file updated for cs test

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,2 +1,2 @@
-export const testLink = "javascript:void(0)";
-// export const testLink = "www.hackerrank.com/ps3-ct-1";
+// export const testLink = "javascript:void(0)";
+export const testLink = "www.hackerrank.com/ps3-ct-1";
